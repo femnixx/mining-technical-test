e837d7f1024e55f07a7315373cccb2cd3580cffe
femnixx <femnitality@gmail.com>
2026-04-15 19:20:17 +0700

    feat: add breeze and sanctum

---
+++ resources/js/Components/MyComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <div style="padding: 20px; background: #f4f4f4;">
+        <h2>Success!</h2>
+        <p>Vue is officially running in Laravel.</p>
+    </div>
+</template>
+
+<script setup>
+</script>
