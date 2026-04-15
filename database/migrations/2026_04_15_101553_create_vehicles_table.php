4bb097059fe765cd73b239f26c0df65d9492728a
femnixx <femnitality@gmail.com>
2026-04-15 17:40:41 +0700

    feat: add columns to migration table for vehicles and approvals

---
+++ database/migrations/2026_04_15_101553_create_vehicles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('model_name');
+            $table->string('plate_number')->unique();
+            $table->enum('type', ['passenger', 'cargo']);
+            $table->enum('ownership', ['owned', 'rented']);
+            $table->foreignId('location_id')->constrained('locations');
+            $table->float('fuel_consumption');
+            $table->date('last_service_at')->nullable();
+            $table->boolean('is_available')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
---
+++ database/migrations/2026_04_15_101605_create_bookings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('vehicle_id')->constrained();
+            $table->foreignId('approver_1_id')->constrained('users');
+            $table->foreignId('approver_2_id')->constrained('users');
+            $table->string('driver_name');
+            $table->enum('status', ['pending', 'approved', 'rejected']);
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
